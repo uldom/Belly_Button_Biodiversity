3016585f8af57bd8c8f2540392208419a02a9ebe
uldom <muldom@gmail.com>
2021-07-25 19:00:43 -0500

    app in progress

---
+++ StarterCode/static/js/app.js
@@ -0,0 +1,27 @@
+// D3 json promise
+d3.json('samples.json')
+    .then(function (data) {
+        init(data)
+    });
+
+// Json organization
+function init(data) {
+    console.log(data.names);
+    console.log(data.metadata);
+    console.log(data.samples);
+    load_dropdown_list(data.names);
+    build_chart('940')
+};
+
+// Dropdown list
+function load_dropdown_list(names) {
+    let dropdown = document.getElementById('selDataset');
+    names.forEach(function (name) {
+        let opt = document.createElement('option');
+        let att = document.createAttribute('value');
+        att.value = name;
+        opt.setAttributeNode(att);
+        opt.text = name;
+        dropdown.appendChild(opt);
+    })
+};
